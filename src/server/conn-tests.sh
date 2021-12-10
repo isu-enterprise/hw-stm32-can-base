6f3c636c9f081c36cf63aa4e3f17b92db8057392
Evgeny Cherkashin <eugeneai@irnok.net>
2021-12-10 20:32:13 +0800

    Tests for FORTH endpoint

--- src/server/conn-tests.sh
+++ src/server/conn-tests.sh
@@ -4,7 +4,13 @@ server=192.168.22.4
 
 set -x
 
-curl http://$server:5000/todos/todo1 -d "data=Remember the milk" -X PUT
-curl http://$server:5000/todos/todo1
-curl http://$server:5000/todos/todo2 -d "data=Change my brakepads" -X PUT
-curl http://$server:5000/todos/todo2
+# curl http://$server:5000/todos/todo1 -d "data=Remember the milk" -X PUT
+# curl http://$server:5000/todos/todo1
+# curl http://$server:5000/todos/todo2 -d "data=Change my brakepads" -X PUT
+# curl http://$server:5000/todos/todo2
+
+# curl http://$server:5000/h3/spi -d '{"type":"string", "message":"HELLO, WORLD!"}' -X POST
+
+curl http://$server:5000/h3/forth -d '{"command":": space 32 emit ;"}' -X POST
+curl http://$server:5000/h3/forth -d '{"command":": space 32 emit1q ;"}' -X POST
+curl http://$server:5000/h3/forth -d '{"command":"4 5 + ."}' -X POST
