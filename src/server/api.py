a0b2866ea4700d8431502158f9c132eda532b3d0
Evgeny Cherkashin <eugeneai@irnok.net>
2021-12-14 19:26:13 +0800

    Copies of classes

--- src/server/api.py
+++ src/server/api.py
@@ -4,11 +4,12 @@ from werkzeug.datastructures import ImmutableMultiDict
 import spi
 import serial
 import time
+import os, signal
 
 TTY='/dev/ttyS1'
 SER=None
 DELAY=40
-
+SER_TIMEOUT=20 # Seconds
 
 app = Flask(__name__)
 api = Api(app)
@@ -54,6 +55,52 @@ class ForthH3(ForthResource):
         return self.send(cmd)
         
 
+class H3Connection(Resource):
+    def post(self):
+        json_data=request.get_json(force=True)
+        typ=json_data['type']
+        msg=json_data['message']
+        print('Received', typ, msg)
+        # spi.tranfer_test()
+        return {'rc':'OK', 'error':0, 'answer':'SOMETHING FROM SPIDEV'}
+
+class ForthResource(Resource):
+    def send(self, cmd):
+        if type(cmd)==str:
+            cmd=cmd.encode('utf8')
+        cmd=cmd.rstrip(b'\n\r')
+        cmd=cmd.rstrip(b'\r\n')
+        cmd=cmd.rstrip(b'\n')
+        cmd=cmd.rstrip(b'\r')
+        SER.write(cmd+b'\r')
+        time.sleep(DELAY/1000)
+        l=SER.readline().decode('utf8')
+        a=l.rstrip('\n')
+        error='NO ERROR'
+        if a.endswith(' ok.'):
+            rc='OK'
+            a=a.rstrip(' ok.')
+        elif (a.endswith(' not found.')):
+            a=a.rstrip(' not found.')
+            error='NOT FOUND'
+            rc='KO'
+        elif (a.endswith(' unhandled interrupt')):
+            # Reset and reload h3
+            a=a.rstrip(' unhandled interrupt')
+            error='UNHANDLED INTERRUPT'
+            c='KO'
+        else:
+            rc='KO'
+            error='UNKNOWN'
+        return {'line':l, 'answer':a, 'rc':rc, 'error':error}
+        
+
+class ForthH3(ForthResource):
+    def post(self):
+        json_data=request.get_json(force=True)
+        cmd=json_data['command']
+        return self.send(cmd)
+
 class ForthFileLoader(ForthResource):
     def post(self):
         content=request.files['content']
@@ -62,18 +109,23 @@ class ForthFileLoader(ForthResource):
         print('here')
         list=[]
         rc='OK'
+        ln=1
+        fln=''
+        qqq
         for l in content:
             l=l.rstrip(b'\n')
-            print(l.decode('utf-8'))
+            print(ln,':', l.decode('utf-8'))
             rcl=self.send(l)
             list.append(rcl)
             if rcl['rc']=='KO':
                 rc='KO'
+                fln=ln
                 break
-            time.sleep(40/1000)
+            # time.sleep(40/1000)
+            ln+=1
         error='NO ERROR'
-        return {'rc':rc, 'list':list, 'error':error}
-
+        return {'rc':rc, 'list':list, 'error':error, 'l':fln}
+    
 class Screen(Resource):
     pass
 
@@ -86,6 +138,16 @@ class H3Connection(Resource):
         # spi.tranfer_test()
         return {'rc':'OK', 'error':0, 'answer':'SOMETHING FROM SPIDEV'}
 
+
+class H3Control(Resource):
+    def post(self):
+        json_data=request.get_json(force=True)
+        cmd = json_data['command']
+        if cmd=='stop':
+            os.kill(os.getpid(), signal.SIGINT)
+        return {'rc:':'OK', 'command':cmd, 'message':'Server is shutting down!'}
+
+    
 class TodoSimple(Resource):
     def get(self, todo_id):
         return {todo_id: todos[todo_id]}
@@ -95,13 +157,14 @@ class TodoSimple(Resource):
         return {todo_id: todos[todo_id]}
 
 api.add_resource(TodoSimple, '/todos/<string:todo_id>')
+api.add_resource(H3Control,  '/h3/control')
 api.add_resource(H3Connection,  '/h3/spi')
 api.add_resource(ForthFileLoader,  '/h3/forth/loader')
 api.add_resource(ForthH3,  '/h3/forth')
 
 if __name__ == '__main__':
     spi.connect()
-    SER = serial.Serial(TTY, 115200, timeout=1)
+    SER = serial.Serial(TTY, 115200, timeout=SER_TIMEOUT)
     app.run(debug=True, host="192.168.22.4")
     spi.disconnect()
     SER.close()
