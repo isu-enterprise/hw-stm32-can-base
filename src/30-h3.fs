380e8e9f91c608c3e399c831ec5a93c38fb8ca5e
Evgeny Cherkashin <eugeneai@irnok.net>
2021-12-15 20:53:06 +0800

    Now we can receive byte data!

--- src/30-h3.fs
+++ src/30-h3.fs
@@ -1,4 +1,4 @@
-\ Connect to Orange Pi PC h3 via SPI2 
+\ Connect to Orange Pi PC h3 via SPI2
 \ SPI2 salve mode for STM
 
 \ USES 00-mmap.fs, spi.fs
@@ -35,7 +35,7 @@ GPIOB constant GPIO.SPI2
 
 : rcc-spi2-enable! ( bool -- )  \ TODO: Check in 25-spi.fs
   RCC RCC.APB1ENR swap \ addr bool
-  1 14 lshift \ addr bool mask
+  true 14 lshift \ addr bool mask
   -rot \ mask addr bool
   if
     bis!
@@ -47,22 +47,22 @@ GPIOB constant GPIO.SPI2
 : spi2-setup-h3 ( -- )
   \ gpioc-setup-ad     \ Setup GPIOC pins to control AD
   spi2-gpio-setup    \ Setup functions on GPIO.SPI2 pins, enable CLK on GPIO.SPI2
-  1 rcc-spi2-enable! \ Enable CLK on SPI2 peripheral
+  true rcc-spi2-enable! \ Enable CLK on SPI2 peripheral
   SPI2
   dup SPI.CR1
     dup 1 6 lshift swap bic!   \ ~SPE, SPI Enable
     dup 1 11 lshift swap bic!  \ 8 bit data frame
 
-    \ dup 1 lshift 9 bic! \ Software management disabled (if ~TI)
-    \ dup 1 lshift 7 bic! \ 0 - MSBFIRST, 1 - LSBFIRST (if ~TI)
-    \ dup 1 lshift 1 bic! \ CPOL (if ~TI)
-    \ dup 1 lshift 0 bis! \ CPHA = 0, L=1  ---\_/-\_/-.....---- SCK \  (if ~TI)
-                          \ CPHA = 1, L=0  _/-\_/-\_/-.....---- SCK \
+    dup 1 9 lshift swap bic! \ Software management disabled (if ~TI)
+    dup 1 7 lshift swap bic! \ 0 - MSBFIRST, 1 - LSBFIRST (if ~TI)
+    dup 1 1 lshift swap bic! \ CPOL (if ~TI)
+    dup 1 0 lshift swap bic! \ CPHA = 0, L=0  ---\_/-\_/-.....---- SCK \  (if ~TI)
+                          \ CPHA = 0, L=0  _/-\_/-\_/-.....---- SCK \
 
     dup 1 2 lshift swap bic!   \ MSTR, SLAVE mode SPI
     drop
   dup SPI.CR2
-    dup 1 4 lshift swap bis!   \ FRF (Frame format) TI mode (seems OK for AD)
+    dup 1 4 lshift swap bic!   \ FRF (Frame format) NO TI mode
     drop
   drop
 ;
@@ -79,7 +79,6 @@ GPIOB constant GPIO.SPI2
     \ %111 5 lshift r@ SPI.CR2 bic! \ disable interrupts
   then
 ;
-
 
 : h3-enable! ( bool -- )
   SPI2 swap spi-enable!
@@ -116,7 +115,6 @@ GPIOB constant GPIO.SPI2
   h3? if h3@ else ." >h3> NO DATA" h3@ then
 ;
 
-
 : SPI2. ( -- )
   SPI2
   cr
@@ -146,11 +144,37 @@ GPIOB constant GPIO.SPI2
   begin
     h3?
     if
-       h3@ . space
+       h3@ hex. space
     else
        \ 42 emit
     then
   key? until
-
 ;
 
+\ Temporal buffer
+create CBUF 1024 allot
+
+: h3-buf-receive ( abuf n -- )
+  \ Receive n bytes via SPI2 into abuf
+  0 ?do
+    begin
+    h3? until
+    h3@ \ abuf val
+    over \ abuf val abuf
+    !
+    1+
+  loop
+  drop \ abuf
+;
+
+: h3-buf. ( abuf n -- )
+  0 ?do
+    dup c@ hex.
+    1+
+  loop
+  drop
+;
+
+: tttt
+  CBUF 20 h3-buf.
+;
